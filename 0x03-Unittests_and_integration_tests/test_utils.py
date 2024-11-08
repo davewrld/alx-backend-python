d83cf5ac61198e32efcbe8729d6e213818bb5fd8
davewrld <keithdave23@gmail.com>
2024-11-08 22:11:16 +0000

    0. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
-"""Module utils"""
+"""Parameterize unittest"""
+
 
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import Mock, patch
 from parameterized import parameterized
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """base test for access_nested_map function"""
+    """Base test for access_nested_map function"""
 
     @parameterized.expand([
-        {"a": 1}, ("a",), 1),
+        ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Test succesful cases"""
+        """Test successful cases"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, exception):
+        """Test exception cases"""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
+
+
+if __name__ == "__main__":
+    unittest.main()
