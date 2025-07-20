69e017b2eb016182d8bbc8349261439b29a65c0f
davewrld <keithdave23@gmail.com>
2025-07-20 15:44:37 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""floor annotations"""
+
+
+def floor(n: float) -> int:
+    """
+        arguments: n
+        returns: float of the floor
+    """
+
+    return int(n // 1)
