03c53d23b46f409f8dd2f6699631067c26744458
davewrld <keithdave23@gmail.com>
2025-07-20 15:56:21 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Take floor(n) as an argument
+    return string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Arg: n
+    return:str (n)
+    """
+
+    return str(n)
