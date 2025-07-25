7022e65c7b569b52fd6d00ddfe9b2938f179ca54
davewrld <keithdave23@gmail.com>
2025-07-25 15:42:15 +0000

    fixed module documentation error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
+"""Type annotated function takes a mixed list
+    of integers and floats
+    """
+
+
 from typing import List, Union
-"""Type annotated function takes a mixed list of integers and floats"""
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """
-        Sums a mixed list
-        Args: integers, floats
-        Returns: sum of list as floats
+    """Sums a mixed list of floats and integers..
+
+    Args:
+        mxd_lst (List[int, float): list of integers and floats.
+
+    Returns:
+        float: Sum all numbers in mxd_lst.
     """
 
     return sum(mxd_lst)
